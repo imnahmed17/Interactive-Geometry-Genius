731737686573df22732d38c8c9900e6196a70119
helloashik17 <asiknoman150@gmail.com>
2023-02-18 10:35:26 +0600

    random background color added

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
 document.getElementById('btn-blog').addEventListener('click', function(){
     window.location.href = 'blog.html';
 });
+
+changeColor = function(e) {
+    e.style.backgroundColor = 'rgb('+Math.round(Math.random()*255)+
+                                            ','+Math.round(Math.random()*255)+
+                                            ','+Math.round(Math.random()*255)+')';
+}
